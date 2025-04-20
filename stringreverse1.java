d232333954774d9b33cc6fd384b3d4d5099dab27
pavan <128489562+pavan44444@users.noreply.github.com>
2025-04-20 11:27:17 +0530

    Create stringreverse1.java
    using StringBuilder().reverse()

---
+++ stringreverse1.java
@@ -0,0 +1,8 @@
+import java.util.* ;
+import java.io.*; 
+public class Solution {	
+	public static String reverseString(String str) {
+		// Write your code here.
+		  return new StringBuilder(str).reverse().toString();
+	}
+}
